fd2c136d119234805a2842f8a05e371cc5e1c685
andersonhcc <programadorandersonh@gmail.com>
2023-01-23 21:29:27 -0300

    configs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",{
+          root: ["./src"],
+          alias:{
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+            '@routes': './src/routes',
+            '@components': './src/components',
+          }
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ]
+    
   };
 };
---
+++ src/screens/SignIn/view.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import {Container} from './styles'
+
+const  SignIn: React.FC = () => {
+
+return <View />;
+
+};
+
+export default  SignIn;
